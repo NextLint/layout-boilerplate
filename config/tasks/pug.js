b56cadb82e9c37703aa8dd635bdbab11cf9ad02a
Ruslan Zolotaryov <zolotaryov.rusik@yandex.ru>
2020-11-09 09:18:01 +0300

    fix(tasks): delete pug
    По началу он будет не нужен

--- config/tasks/pug.js
+++ config/tasks/pug.js
@@ -1,10 +1,16 @@
 const { src, dest } = require('gulp')
-const pug = require('gulp-pug')
+// const pug = require('gulp-pug')
 const plumber = require('gulp-plumber')
 
-module.exports = function pugHtml() {
-    return src('src/app/*.pug')
+// module.exports = function html() {
+//     return src('src/app/*.pug')
+//         .pipe(plumber())
+//         .pipe(pug())
+//         .pipe(dest('build'))
+// }
+
+module.exports = function html() {
+    return src('src/app/*.html')
         .pipe(plumber())
-        .pipe(pug())
         .pipe(dest('build'))
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const { series } = require('gulp')
 
 const clean = require('./config/tasks/clean.js')
-const pugHtml = require('./config/tasks/pug.js')
+const html = require('./config/tasks/pug.js')
 const styles = require('./config/tasks/styles.js')
 const scripts = require('./config/tasks/scripts.js')
 const images = require('./config/tasks/images.js')
@@ -9,7 +9,7 @@ const fonts = require('./config/tasks/fonts.js')
 // const svgSprite = require('./tasks/svgScprite.js')
 const server = require('./config/tasks/server.js')
 
-const build = series(clean, pugHtml, styles, scripts, images, fonts)
+const build = series(clean, html, styles, scripts, images, fonts)
 
 exports.build = series(build)
 exports.raise = series(build, server)
